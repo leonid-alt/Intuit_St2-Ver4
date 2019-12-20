bb77485a5a321a10bc039fbb482fee396467ab52
leonid <stepalt.22@gmail.com>
2019-12-20 13:13:48 +0700

    дыб структуры и перечисления

--- Intuit_St2/AppDelegate.m
+++ Intuit_St2/AppDelegate.m
@@ -18,11 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    
-    
-    
     // Строки
-    
     NSString *a = @"abc";
     NSString *b = [a stringByReplacingOccurrencesOfString:@"a"
                                                withString:@"b"];
@@ -49,7 +45,6 @@
         NSLog(@"%@", str);
     }
     
-    
     // Создание словаря объектов по "ключь : значение"
     // и вывод на печать:  key2 - b,  key1 - a
     NSDictionary *dict = @{ @"key1" : @"a", @"key2" : @"b" };
@@ -59,12 +54,62 @@
         NSLog(@"%@ - %@", key, value);
     }
     
+    // Структуры
+    CGPoint p;
+    p.x = 10;
+    p.y = 20;
+    p = CGPointMake(10, 20);
+    
+    CGSize s;
+    s.width = 100;
+    s.height = 100;
+    s = CGSizeMake(100, 100);
+    
+    CGRect u;
+    u.size = s;
+    u.origin = p;
+    u = CGRectMake(10, 20, 100, 100);
+    
+    // Структуры
+    typedef struct
+    {
+        float x;
+        float y;
+    } Location;
+    
+    /*
+    Location createLocation(float x, float y)
+    {
+        Location l;
+        l.x = x;
+        l.y = y;
+        return l;
+    }
+    
+    int main() {
+        Location l = createLocation(1.5, 0.5);
+        printf("location: {%g, %g}", l.x, l.y);
+    }
+    */
+    
+    // Перечисления
+    typedef enum
+    {
+        UIViewAnimationCurveEaseInOut,
+        UIViewAnimationCurveEaseIn,
+        UIViewAnimationCurveEaseOut,
+        UIViewAnimationCurveLinear
+    } UIViewAnimationCurve;
+    
+    UIViewAnimationCurve t = UIViewAnimationCurveLinear;
+    printf("%d", t);
+    
     
     // Выделение памяти (alloc) и инициализация (init)  класса User
-    User *u = [[User alloc] init];
+    User *q = [[User alloc] init];
     
     // Вывод в консоль user: <User: 0x7a74bef0>
-    NSLog(@"user: %@", u);
+    NSLog(@"user: %@", q);
     
     // Override point for customization after application launch.
     return YES;
